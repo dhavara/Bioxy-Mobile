f1e590c0ef07e8583c5832c6f478474d37cdd063
Enrico <enricowakiman9@gmail.com>
2022-01-08 17:29:14 +0700

    yo adapter is back bro

--- app/src/main/java/com/keld/bioxy/view/LeaderboardView/LeaderboardAdapter.java
+++ app/src/main/java/com/keld/bioxy/view/LeaderboardView/LeaderboardAdapter.java
@@ -1,12 +1,18 @@
 package com.keld.bioxy.view.LeaderboardView;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.keld.bioxy.R;
 import com.keld.bioxy.model.Leaderboard;
 
 import java.util.List;
@@ -29,12 +35,19 @@ public class LeaderboardAdapter extends RecyclerView.Adapter<LeaderboardAdapter.
     @NonNull
     @Override
     public CardViewViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.card_leaderboard, parent, false);
+        return new CardViewViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CardViewViewHolder holder, int position) {
-
+        final Leaderboard.Leaderboards results = getLeaderboardsList().get(position);
+        holder.lb_rank.setText(results.getId());
+        holder.lb_difficulty.setText(results.getDifficulty());
+        holder.lb_username.setText(results.getUsername());
+        holder.lb_point.setText(results.getPoint());
+        holder.lb_accuracy.setText(results.getAccuracy());
+        holder.lb_date.setText(results.getCreated_at());
     }
 
     @Override
@@ -50,8 +63,17 @@ public class LeaderboardAdapter extends RecyclerView.Adapter<LeaderboardAdapter.
     }
 
     public class CardViewViewHolder extends RecyclerView.ViewHolder {
+        TextView lb_rank, lb_difficulty, lb_username, lb_point, lb_accuracy, lb_date;
+        CardView cardView;
         public CardViewViewHolder(@NonNull View itemView) {
             super(itemView);
+            lb_rank = itemView.findViewById(R.id.lb_rank);
+            lb_difficulty = itemView.findViewById(R.id.lb_difficulty);
+            lb_username = itemView.findViewById(R.id.lb_username);
+            lb_point = itemView.findViewById(R.id.lb_point);
+            lb_accuracy = itemView.findViewById(R.id.lb_accuracy);
+            lb_date = itemView.findViewById(R.id.lb_date);
+            cardView = itemView.findViewById(R.id.cv_layout_leaderboard);
         }
     }
 }
