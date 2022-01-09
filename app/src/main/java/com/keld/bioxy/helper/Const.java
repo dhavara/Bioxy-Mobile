d8ac618ebe7cef39651c10a4afb4fba361fb38e1
dhavara <dhava.rachmana.aziz@gmail.com>
2022-01-09 13:41:28 +0700

    nambah logo (lagi)

--- app/src/main/java/com/keld/bioxy/helper/Const.java
+++ app/src/main/java/com/keld/bioxy/helper/Const.java
@@ -1,6 +1,6 @@
 package com.keld.bioxy.helper;
 
 public class Const {
-    public final static String BASE_URL = "http://192.168.100.10/Bioxy-Web/public/api/";
-    public final static String IMG_URL = "http://192.168.100.10/Bioxy-Web/public/img/";
+    public final static String BASE_URL = "http://192.168.0.9/Bioxy-Web/public/api/";
+    public final static String IMG_URL = "http://192.168.0.9/Bioxy-Web/public/img/";
 }
