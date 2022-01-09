016b9a544194fe436f93a0ea3db05bdbcedf0c8c
Enrico <enricowakiman9@gmail.com>
2022-01-09 19:11:21 +0700

    amogusbrangus5

--- app/src/main/java/com/keld/bioxy/view/LeaderboardView/LeaderboardAdapter.java
+++ app/src/main/java/com/keld/bioxy/view/LeaderboardView/LeaderboardAdapter.java
@@ -43,8 +43,11 @@ public class LeaderboardAdapter extends RecyclerView.Adapter<LeaderboardAdapter.
     @Override
     public void onBindViewHolder(@NonNull CardViewViewHolder holder, int position) {
         final Leaderboard.Leaderboards results = getLeaderboardsList().get(position);
-        int ranking = 1;
+        int ranking = 0;
         if (results.getDifficulty() == "Mudah") {
+            if (ranking == 0) {
+                ranking = 1;
+            }
             ranking += 1;
         }
         if (results.getDifficulty() == "Sedang") {
